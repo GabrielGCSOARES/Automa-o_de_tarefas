f087fdbce3d68fe80ccb5b144f754fb00c510c5e
GabrielGCSOARES <gs519636@gmail.com>
2025-06-03 19:48:24 -0300

    Patch 0.1

---
+++ codigo.py
@@ -0,0 +1,5 @@
+# Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# Passo 2: Fazer o login
+# Passo 3; Importar a base de dados
+# Passo 4: fazer o cadastro do produtos sendo seu código,marca, tipo,categoria, preço,custo,OBS
+# Passo 5: Repetir para todos os produtos 
