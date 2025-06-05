1b97a8fd6454db0fb1cb755bbbd1c17e93fe349d
GabrielGCSOARES <gs519636@gmail.com>
2025-06-04 23:54:18 -0300

    patch 0.3

--- codigo.py
+++ codigo.py
@@ -1,5 +1,7 @@
 import pyautogui 
 import time 
+
+
 pyautogui.PAUSE = 1
 
 
@@ -16,8 +18,19 @@ time.sleep(3)
 
 # Passo 2: Fazer o login
 pyautogui.click(x=649, y=477)
+pyautogui.write("malenia@email.com")
+time.sleep(0.5)
+pyautogui.press("tab")
+pyautogui.write("minhasasd")
+pyautogui.press("tab")
+pyautogui.press("enter")
 
+time.sleep(3)
 # Passo 3; Importar a base de dados
+import pandas 
+pandas.read_csv("produtos.csv")
+
+
 # Passo 4: fazer o cadastro do produtos sendo seu código,marca, tipo,categoria, preço,custo,OBS
 # Passo 5: Repetir para todos os produtos 
 
